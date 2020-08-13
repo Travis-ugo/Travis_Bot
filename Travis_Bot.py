c7334dca33a654112ea9478474ad0317d5a98eaf
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-13 11:19:50 +0100

    Use flask and flask import request

--- Travis_Bot.py
+++ Travis_Bot.py
@@ -1,8 +1,15 @@
 import telebot
- 
+import os 
+import flask
+from flask import request
 
-Bot_API_TOKEN = "1251418314:AAGaPAc7pcpKWzluP-PYYhGk9LbcSMtbcCM"
-bot = telebot.TeleBot(Bot_API_TOKEN)
+server = flask.Flask(__name__)
+
+debug = False
+token = "1251418314:AAGaPAc7pcpKWzluP-PYYhGk9LbcSMtbcCM"
+
+bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -42,4 +49,29 @@ def handle_video(message):
     id = message.from_user.id
     bot.send_video(chat_id=id , data = 'https://res.cloudinary.com/thrinitee/video/upload/v1597241738/meek_mill_dangerous_feat._jeremih_pnb_rock_h264_50611_xogq88.mp4')
 
-bot.polling()
+
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    request_object = request.stream.read().decode('utf-8')
+    update_to_json = [telebot.types.Update.de_json(request_object)]
+    bot.process_new_updates(update_to_json)
+    return 'whats good'
+
+
+@server.route('/hook')
+def Webhook():
+    url = 'https://okonicha-bot.herokuapp.com/'
+    bot.remove_webhook()
+    bot.set_webhook(url + token)
+    return f'webhook set to url {url}'
+
+@server.route('/')
+def good():
+    return f'hello funny boy'
+
+if debug == True:
+    bot.remove_webhook()
+    bot.polling()
+else:
+    if __name__== '_main_':
+        server.run(host='0.0.0.0', port=int(os.environ.get('POST', 5000)))

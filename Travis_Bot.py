a94a60929a9b3141dbbb8cea29d718aa190822cf
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-13 18:57:17 +0100

    port

--- Travis_Bot.py
+++ Travis_Bot.py
@@ -74,4 +74,4 @@ if debug == True:
     bot.polling()
 else:
     if __name__== "__main__":
-        server.run(host="0.0.0.0", port=int(os.environ.get('POST', 5000)))
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
